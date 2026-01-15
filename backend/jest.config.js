a7233595e543915f289ce1c8f75b88e2eaca8f79
mare555 <mare555@users.noreply.github.com>
2026-01-15 07:45:57 +0000

    deploy: 10ea05fd2748f7a82d6569b6309b99f48c425d82

--- backend/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage/backend',
-  coverageReporters: ['lcov', 'text']
-};
--- frontend/src/__tests__/Layout.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Layout from '../components/Layout'
-
-describe('Layout component', () => {
-  test('renders children content', () => {
-    render(<Layout><div>Inner</div></Layout>)
-    expect(screen.getByText('Inner')).toBeTruthy()
-  })
-})
